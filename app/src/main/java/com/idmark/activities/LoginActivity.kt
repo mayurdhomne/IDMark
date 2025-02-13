99d538af8544a0296977c8c702eaedb6ea6e3d1b
mayurdhomne <133950762+mayurdhomne@users.noreply.github.com>
2025-02-13 10:41:47 +0530

    2nd commit login ui

---
+++ app/src/main/java/com/idmark/activities/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.idmark.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.idmark.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+    }
+}
